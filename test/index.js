c96a23d4a0a94f62de37081b2346806a222c03e4
Jason Miller <jason@developit.ca>
2016-01-07 15:21:19 -0500

    Update tests for iframe + attribute changes

--- test/index.js
+++ test/index.js
@@ -11,7 +11,23 @@ describe('preact-richtextarea', () => {
 
 		it('should render the correct DOM', () => {
 			expect(<RichTextArea />).to.eql(
-				<richtextarea class="preact-richtextarea" is-placeholder="false" contentEditable="true" />
+				<richtextarea class="preact-richtextarea" tabIndex=" ">
+					<iframe />
+				</richtextarea>
+			);
+		});
+
+		it('should render placeholders', () => {
+			expect(<RichTextArea placeholder="test" />).to.eql(
+				<richtextarea class="preact-richtextarea" tabIndex=" " is-placeholder="true">
+					<iframe />
+				</richtextarea>
+			);
+
+			expect(<RichTextArea placeholder="test" value="foo bar" />).to.eql(
+				<richtextarea class="preact-richtextarea" tabIndex=" ">
+					<iframe />
+				</richtextarea>
 			);
 		});
 	});
