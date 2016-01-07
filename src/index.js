01a379f2d7133958448b7b435d39c28969db26db
Jason Miller <jason@developit.ca>
2016-01-07 15:21:07 -0500

    Switch to an iframe for encapsulating rich text so that styles dont leak

--- src/index.js
+++ src/index.js
@@ -1,25 +1,148 @@
 import { h, Component } from 'preact';
 import './style.css';
 
+const UID = Math.random().toString(32).substring(2);
+
+const EMPTY_VALUE = '<br>';
+
 export default class RichTextArea extends Component {
-	shouldComponentUpdate(props) {
-		for (let i in props) if (i!=='value' && props[i]!==this.props[i]) return true;
-		// value hook is special: contentEditable freaks out when we overwrite innerHTML, so lets avoid it by doing a manual update.
-		if (props.value!==this.props.value) {
-			this.props.value = props.value;
-			this.componentDidUpdate();
-		}
+	constructor(props) {
+		super(props);
+		this.componentDidUpdate = this.componentDidUpdate.bind(this);
+		this.updateHeight = this.updateHeight.bind(this);
+		this.handleEvent = this.handleEvent.bind(this);
+		this.doFocus = this.doFocus.bind(this);
+	}
+
+	exec(command, ...args) {
+		let doc = this.getDocument();
+		if (doc) return doc[command](...args);
+	}
+
+	execCommand(command, ...args) {
+		return this.exec('execCommand', command, ...args);
+	}
+
+	queryCommandState(command) {
+		return this.exec('queryCommandState', command);
+	}
+
+	queryCommandValue(command) {
+		return this.exec('queryCommandValue', command);
+	}
+
+	componentDidMount() {
+		this.updateHeightTimer = setInterval(this.updateHeight, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.updateHeightTimer);
+	}
+
+	shouldComponentUpdate({ value, stylesheet, placeholder, ...props }) {
+		for (let i in props) if (props[i]!==this.props[i]) return true;
+		this.props = { ...this.props, value, placeholder, stylesheet };
+		this.componentDidUpdate();
 		return false;
 	}
 
 	componentDidUpdate() {
-		let value = this.props.value || '',
-			current = this.base.innerHTML;
-		if (this.placeholderShowing===true && current===this.props.placeholder) current = '';
+		clearTimeout(this.updateTimer);
+		this.setup();
+
+		let editor = this.getEditor();
+		if (!editor) {
+			this.editorRetries = (this.editorRetries || 0) + 1;
+			this.updateTimer = setTimeout(this.componentDidUpdate, this.editorRetries>10 ? 100 : 1);
+			return;
+		}
+		this.editorRetries = 0;
+
+		let value = this.props.value || EMPTY_VALUE,
+			stylesheet = this.props.stylesheet,
+			current = editor.innerHTML;
+
+		if (stylesheet!==this.stylesheet) {
+			this.setStyleSheet(stylesheet);
+		}
+
+		if (this.placeholderShowing===true && current===this.props.placeholder) {
+			current = EMPTY_VALUE;
+		}
 		if (current!==value) {
-			this.base.innerHTML = value;
+			editor.innerHTML = value;
 		}
 		this.updatePlaceholder();
+		this.updateHeight();
+	}
+
+	setStyleSheet(stylesheet) {
+		this.stylesheet = stylesheet;
+		let doc = this.getDocument(),
+			s = doc.getElementById('prtcss'+UID);
+		if (s) s.parentNode.removeChild(s);
+
+		let head = doc.head || doc.getElementsByTagName('head')[0];
+		if (!head) head = doc.body.parentNode.insertBefore(doc.createElement('head'), doc.body);
+
+		s = doc.createElement('style');
+		s.setAttribute('id', 'prtcss'+UID);
+		s.appendChild(doc.createTextNode(stylesheet));
+		head.appendChild(s);
+	}
+
+	setup() {
+		let doc = this.getDocument();
+		if (!doc || (doc.body && doc.body._hasbeensetup===true)) return;
+
+		if (!doc.body) {
+			doc.open();
+			doc.write('<!DOCTYPE html><html><body contentEditable></body></html>');
+			doc.close();
+		}
+		doc.designMode = 'on';
+		doc.documentElement.style.cursor = 'text';
+		doc.documentElement.style.overflowY = doc.body.style.overflowY = 'hidden';
+		doc.body.style.minHeight = '1.2em';
+		doc.body.contentEditable = true;
+		doc.body._hasbeensetup = true;
+		let win = this.getFrame().contentWindow;
+		win.onfocus = win.onblur = win.oninput = win.onchange = this.handleEvent;
+		win.onscroll = win.onload = this.updateHeight;
+	}
+
+	updateHeight() {
+		clearTimeout(this.uht);
+		this.uht = null;
+		let doc = this.getDocument(),
+			br = doc && doc._br;
+		if (!doc) return;
+		if (!br) {
+			br = doc._br = doc.createElement('div');
+			br.style.cssText = 'position:relative;overflow:hidden;clear:both;';
+		}
+		doc.body.appendChild(br);
+		let ph = br.offsetTop + doc.documentElement.offsetHeight - doc.body.offsetHeight;
+		doc.body.removeChild(br);
+
+		let frame = this.getFrame();
+		if (ph!==frame.offsetHeight) {
+			frame.style.height = ph+'px';
+		}
+	}
+
+	getFrame() {
+		return this.base && this.base.firstChild;
+	}
+
+	getDocument() {
+		let frame = this.getFrame();
+		return frame && frame.contentWindow && frame.contentWindow.document;
+	}
+
+	getEditor() {
+		let doc = this.getDocument();
+		return doc && doc.body;
 	}
 
 	getHandler(type) {
@@ -28,36 +151,60 @@ export default class RichTextArea extends Component {
 
 	handleEvent({ type, target }) {
 		let fn = this.getHandler(type),
-			value = this.base.innerHTML;
-		if (type==='focus' || type==='blur') this.focussed = type==='focus';
-		this.updatePlaceholder();
+			editor = this.getEditor(),
+			value = editor.innerHTML;
+		target = editor || target;
+		if (type==='focus' || type==='blur') {
+			this.focussed = type==='focus';
+			this.updatePlaceholder();
+			if (type==='focus') target.focus();
+		}
 		if (fn) fn({ value, type, target, currentTarget:this });
+		if (!this.uht) {
+			this.uht = setTimeout(this.updateHeight, 20);
+		}
 	}
 
 	updatePlaceholder() {
 		let { placeholder } = this.props,
-			value = this.base.innerHTML,
-			show = (!value || value===placeholder) && placeholder && !this.focussed;
+			editor = this.getEditor(),
+			value = editor.innerHTML,
+			norm = this.normalizeEmptyValue(value),
+			show = (!norm || norm===EMPTY_VALUE || norm===placeholder) && placeholder && !this.focussed;
+
 		if (show===this.placeholderShowing) return;
 		this.placeholderShowing = show;
+
 		if (show) {
-			if (!value) this.base.innerHTML = placeholder;
-			this.base.setAttribute('is-placeholder', true);
+			if (!norm) editor.innerHTML = placeholder;
+			this.base.setAttribute('is-placeholder', 'true');
 		}
 		else {
-			if (value===placeholder) this.base.innerHTML = '';
+			if (value===placeholder) editor.innerHTML = EMPTY_VALUE;
 			this.base.removeAttribute('is-placeholder');
 		}
 	}
 
-	render({ value, class:cl, className, ...props }) {
-		let evt = e => this.handleEvent(e);
+	normalizeEmptyValue(value) {
+		return typeof value==='string' ? value.replace(/^[\s\n]*?(<br\s*?\/?>)?[\s\n]*?$/gi, '') : '';
+	}
+
+	doFocus(e) {
+		let a = this.getEditor();
+		if (a) a.focus();
+		if (e) return e.preventDefault(), false;
+	}
+
+	render({ value, class:cl, className, placeholder, stylesheet, ...props }) {
+		for (let i in props) if (props.hasOwnProperty(i) && i.match(/^on/i) && typeof props[i]==='function') delete props[i];
 		return (
 			<richtextarea class={{
 				'preact-richtextarea': true,
 				[cl]: cl,
 				[className]: className
-			}} {...props} is-placeholder={!value && !!props.placeholder} contentEditable onFocus={evt} onBlur={evt} onInput={evt} onChange={evt} />
+			}} {...props} onFocus={this.doFocus} tabIndex=" " is-placeholder={ (!value && !!placeholder) || null }>
+				<iframe />
+			</richtextarea>
 		);
 	}
 }
